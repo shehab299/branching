a4994114276013ab6ad481a9b408dab941316a45
shehab299 <shehab299@outlook.com>
2023-10-27 18:04:39 +0200

    first commit in branch calc

--- code.py
+++ code.py
@@ -14,3 +14,14 @@ def mul(x,y)
 {
     return x*y
 }
+
+
+def calc(x,y ,ch)
+{
+    if(ch == 'a')
+        return add(x,y)
+    elif(ch == 'm')
+        return mul(x,y)
+    elif(ch == 'p')
+        return pow(x,y)
+}
